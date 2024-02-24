5d444a56bf0a49fe3d640baffb7d94b6a1724e05
Erxiel <votexgamer281@gmail.com>
2024-02-23 19:26:30 -0600

    Se hicieron los cambios en los test, ahora funcionan correctamente

--- src/main/kotlin/design/patterns/calculadora/Calculadora.kt
+++ src/main/kotlin/design/patterns/calculadora/Calculadora.kt
@@ -2,22 +2,22 @@ package design.patterns.design.patterns.calculadora
 
 class Calculadora{
 
-    fun sumar(numero1:Float, numero2:Float): Float{
+    fun sumar(numero1: Float, numero2: Float): Float {
         return numero1 + numero2
     }
 
-    fun restar(numero1:Float, numero2:Float): Float{
+    fun restar(numero1: Float, numero2: Float): Float {
         return numero1 - numero2
     }
 
-    fun multiplicacion(numero1:Float, numero2:Float): Float{
+    fun multiplicacion(numero1: Float, numero2: Float): Float {
         return numero1 * numero2
     }
 
-    fun division(numero1:Float, numero2:Float):Float{
-        if (numero2 != 0.0f){
+    fun division(numero1: Float, numero2: Float): Float {
+        if (numero2 != 0.0f) {
             return numero1 / numero2
-        }else{
+        } else {
             throw IllegalArgumentException("No se puede dividir por cero")
         }
     }
--- src/test/kotlin/design/patterns/calculadoraTest/CalculatorTest.kt
+++ src/test/kotlin/design/patterns/calculadoraTest/CalculatorTest.kt
@@ -6,36 +6,32 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 class CalculatorTest {
     private val calculadora = Calculadora()
-
     @Test
-    fun testSumar() {
-        assertEquals(8.0, calculadora.sumar(5.0, 3.0))
-        assertEquals(0.0, calculadora.sumar(0.0, 0.0))
-        assertEquals(-2.0, calculadora.sumar(-5.0, 3.0))
+    fun `Calcular la suma de dos valores`() {
+        val numero1: Float = 10.0f
+        val numero2: Float = 25.23f
+        assertEquals(35.23f, calculadora.sumar(numero1, numero2))
     }
 
     @Test
-    fun testRestar() {
-        assertEquals(2.0, calculadora.restar(5.0, 3.0))
-        assertEquals(0.0, calculadora.restar(5.0, 5.0))
-        assertEquals(-8.0, calculadora.restar(-5.0, 3.0))
+    fun `Calcular la resta de dos valores`() {
+        val numero1: Float = 10.0f
+        val numero2: Float = 25.23f
+        assertEquals(-15.23f, calculadora.restar(numero1, numero2))
     }
 
     @Test
-    fun testMultiplicar() {
-        assertEquals(15.0, calculadora.multiplicar(5.0, 3.0))
-        assertEquals(0.0, calculadora.multiplicar(5.0, 0.0))
-        assertEquals(-15.0, calculadora.multiplicar(-5.0, 3.0))
+    fun `Calcular la multiplicacion de dos valores`() {
+        val numero1: Float = 1.0f
+        val numero2: Float = 2.0f
+        assertEquals(2.0f, calculadora.multiplicacion(numero1, numero2))
     }
 
     @Test
-    fun testDividir() {
-        assertEquals(1.6666666666666667, calculadora.dividir(5.0, 3.0))
-        assertEquals(Double.POSITIVE_INFINITY, calculadora.dividir(5.0, 0.0))
-        assertEquals(-1.6666666666666667, calculadora.dividir(-5.0, 3.0))
-
-        assertFailsWith<IllegalArgumentException> { calculadora.dividir(5.0, 0.0) }
+    fun `Calcular la division de dos valores`() {
+        val numero1: Float = 6.0f
+        val numero2: Float = 2.0f
+        assertEquals(3.0f, calculadora.division(numero1, numero2))
     }
-
 
 }
